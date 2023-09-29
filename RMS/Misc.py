2a051897bbd7b626cb990187278d080231fc460e
David Rollinson <g7gpr@outlook.com>
2023-09-29 13:20:45 +0000

    Remove debug loggin

--- RMS/Misc.py
+++ RMS/Misc.py
@@ -594,7 +594,7 @@ def sanitise(unsanitised, lower = False, space_substitution = "", log_changes = 
             if c in permitted:
                 sanitised += c
 
-    log.info("String {} was sanitised to {}".format(unsanitised, sanitised))
+
     if unsanitised != sanitised and log_changes:
         log.info("String {} was sanitised to {}".format(unsanitised, sanitised))
 
