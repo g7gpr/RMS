89ff8412235d6174a20c196237b6adbc23727245
David Rollinson <g7gpr@outlook.com>
2024-01-06 01:20:08 +0000

    Sort for improved performance

--- Utils/ReadGAIACatalog.py
+++ Utils/ReadGAIACatalog.py
@@ -1,93 +1,156 @@
-
 import os
 import numpy as np
+import argparse
+from tqdm import tqdm
+import pickle
+import urllib.request
+import time
+
+# Read from http://jvo.nao.ac.jp/portal/gaia/dr3.do
+# Photometric calculations at https://gea.esac.esa.int/archive/documentation/GDR3/Data_processing/chap_cu5pho/cu5pho_sec_photSystem/cu5pho_ssec_photRelations.html
+
+# Example star name query
+# http://simbad.cds.unistra.fr/simbad/sim-id?Ident=Gaia+DR3+1137162861578295168
 
 
-def readGaiaCatalogTxt(file_path, lim_mag=None):
-    """ Read star data from the GAIA catalog in the .dat format. """
+def handleHeader(line):
+
+    print("Handling {}".format(line))
+    if "condition" in line:
+        condition = line.split(':')[1]
+        print("Condition {}".format(condition))
+        return "Condition", condition
+    elif "columns:" in line:
+        columns_list_raw = line.split(':')[1].split(",")
+        columns_list = []
+        for col_name in columns_list_raw:
+            columns_list.append(col_name.strip())
+        return "Columns", columns_list
+    elif "totalCount" in line:
+        object_count = line.split(':')[1]
+        return "Object_count", object_count
+
+def readGaiaCatalogTxt(file_path, lim_mag=None, header_designator = "#", max_objects = None):
+    """ Read star data from the GAIA catalog in the .psv format - works with any column order. """
 
 
     results = []
+    print(file_path)
+    # intialise
+
+    condition, columns, object_count = None, None, None
+
+    if max_objects is None:
+        max_objects = np.inf
+    else:
+        max_objects = max_objects[0]
+        print("Only reading {} objects".format(max_objects))
 
     with open(file_path) as f:
 
-        # Skip the header
-        f.next()
-        f.next()
+
+        # Initialise list to hold the catalogue
+        catalogue, objects = [],0
 
         # Read Gaia catalog
-        for line in f:
 
-            line = line.replace('\n', '').replace('\r', '')
-            line = line.split('|')
+        for line in tqdm(f):
 
-            ra, dec, mag = list(map(float, line))
+            if line[0]==header_designator:
+                info_type, data_returned = handleHeader(line[1:])
+                condition = data_returned if info_type == "Condition" else condition
+                columns = data_returned if info_type == "Columns" else columns
+                object_count = data_returned if info_type == "Object_count" else object_count
+                continue
 
-            # Skip magnitudes below the limiting magnitude
-            if lim_mag is not None:
-                if mag < lim_mag:
-                    continue
+            catalogue.append(line.split('|'))
+            objects += 1
+            if objects > max_objects:
+                break
 
+        # sort by designation - also known is gaia id
 
-            results.append([ra, dec, mag])
+        designator_column_number = columns.index("designation")
 
-
-
-        return np.array(results)
-
-
-
-def loadGaiaCatalog(dir_path, file_name, lim_mag=None):
-    """ Read star data from the GAIA catalog in the .npy format. 
-    
-    Arguments:
-        dir_path: [str] Path to the directory where the catalog file is located.
-        file_name: [str] Name of the catalog file.
-
-    Keyword arguments:
-        lim_mag: [float] Faintest magnitude to return. None by default, which will return all stars.
-
-    Return:
-        results: [2d ndarray] Rows of (ra, dec, mag), angular values are in degrees.
-    """
-
-    file_path = os.path.join(dir_path, file_name)
-
-    # Read the catalog
-    results = np.load(file_path, allow_pickle=False)
+        catalogue_sorted_by_gaia_id = sorted(catalogue,key=lambda object_line: object_line[designator_column_number])
+        print("Object_count {}".format(object_count))
+        print("Catalogue_length {}".format(len(catalogue)))
 
 
-    # Filter by limiting magnitude
-    if lim_mag is not None:
+        return catalogue_sorted_by_gaia_id, columns
 
-        results = results[results[:, 2] < lim_mag]
+def getStarNames(catalogue):
+
+    star_list, designator_list = [],[]
+    for star in catalogue:
+
+        (star[columns.index("designation")].replace(" ", "+"))
+        designator = star[columns.index("designation")]
+        designator_list.append(designator)
 
 
-    return results
+    cross_reference_list = []
+    with open("/home/david/tmp/IDS.txt",'r') as fh:
+        print(designator_list)
+        for line in fh:
+            ident_list = line.replace('"','').split("|")
+
+            for ident in ident_list:
+                if "DR3" in ident:
+                    gaia_dr3_ident = ident
+                    if ident in designator_list:
+                        cross_reference_list.append([gaia_dr3_ident, ident_list[-1].strip()])
 
 
-            
-
-
-    
+    # sort by gaia ident to speed up the future join
+    cross_reference_list_sorted_by_id = sorted(cross_reference_list,key=lambda gaia2sinbadcode: gaia2sinbadcode[0])
+    return cross_reference_list_sorted_by_id
 
 
 if __name__ == "__main__":
 
-    import matplotlib.pyplot as plt
-
-    # file_path = 'gaia_dr2.dat'
-    # results = readGaiaCatalogTxt(file_path)
+    ### COMMAND LINE ARGUMENTS
 
 
-    file_name = 'gaia_dr2_mag_11.5.npy'
 
-    results = loadGaiaCatalog('.', file_name, lim_mag=7)
+    # Init the command line arguments parser
 
-    #np.save('gaia_dr2_mag_11.5', results, allow_pickle=False)
+    arg_parser = argparse.ArgumentParser(description="Tool for encoding GAIA DR3 information into RMS format.")
 
-    ra, dec, mag = results.T
+    arg_parser.add_argument('input_path', metavar='INPUT_FILE', type=str,
+                            help='Path to the input file.')
+
+    arg_parser.add_argument('-m', '--maxobjects', nargs=1, metavar='MAX_OBJECTS', type=int,
+                            help="The maximum number of objects to read, useful for debugging.")
+
+    cml_args = arg_parser.parse_args()
 
 
-    plt.scatter(ra, dec, s=0.1)
-    plt.show()
+
+    print("Reading file from {}".format(cml_args.input_path))
+
+
+
+
+    catalogue, columns = readGaiaCatalogTxt(os.path.expanduser(cml_args.input_path),max_objects=cml_args.maxobjects)
+
+    with open('/home/david/tmp/catalogue.pickle', 'wb') as fh:
+        pickle.dump(catalogue, fh)
+
+    print(columns)
+
+    gaia2name_list = getStarNames(catalogue)
+
+    with open("/home/david/tmp/gaia2id.txt", 'w') as fh:
+
+        for pair in gaia2name_list:
+            fh.write("{}|{}\n".format(pair[0],pair[1]))
+
+
+
+
+    #ra, dec, mag = results.T
+
+
+    #plt.scatter(ra, dec, s=0.1)
+    #plt.show()
