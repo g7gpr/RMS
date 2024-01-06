415e5114d82edb5b00d09033ff68027bd9d971be
g7gpr <g7gpr@outlook.com>
2024-01-06 23:22:09 +0800

    Debugging

--- Utils/ReadGAIACatalog.py
+++ Utils/ReadGAIACatalog.py
@@ -349,6 +349,7 @@ def generateDR3CatalogueWithSimbadCode(gaia_catalogue, gaia_columns, name_list, 
 
         #this needs to be sorted by oid
         print("gaia_columns")
+        print(gaia_columns)
         oid_col_no = gaia_columns.index('oid')
         print("oid_col_no".format(oid_col_no))
         catalogue_with_oid_sorted_oid = sorted(catalogue_with_oid, key=lambda oid: oid[oid_col_no])
