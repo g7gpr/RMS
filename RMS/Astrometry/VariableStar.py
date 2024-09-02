5047d6f6fca970add232cb6ce357ebce05b80a82
David Rollinson <g7gpr@outlook.com>
2024-09-02 23:58:08 +0000

    Commenting

--- RMS/Astrometry/VariableStar.py
+++ RMS/Astrometry/VariableStar.py
@@ -1,4 +1,5 @@
-""" This module contains procedures for detecting variations in star magnitude.
+""" This module contains procedures for collating data from multiple stations in a database
+    of magnitudes and can produce a chart of magnitudes close to radec coordinates
 """
 
 # The MIT License
@@ -84,53 +85,82 @@ def readInArchivedCalstars(config, conn):
 
     """
     Iterates over the ArchivedDirectories for the station to load all
-    the calstar files into memory
+    the calstar files into the database in radec format
 
     Args:
-        config (): config file
-
+        config(): config instance
+        conn(): database connection instance
     Returns:
 
     """
 
+    # Load the star catalogue
     catalogue = loadGaiaCatalog("~/source/RMS/Catalogs", "gaia_dr2_mag_11.5.npy", lim_mag=11)
+
+    # Deduce the path to the archived directories for this station
     archived_directories_path = os.path.join(config.data_dir, config.archived_dir)
     archived_directories = getNightDirs(archived_directories_path, config.stationID)
+
+    # Reverse this list so that the newest directories are at the front
     archived_directories.reverse()
 
-
-    print("Archived Directories")
-    calstar_list = []
+    # Find the most recent jd in the database for this station
     latest_jd = findMostRecentEntry(config, conn)
-    archived_directories_filtered_by_jd = []
+
+    # Initialise the calstar list
+    calstar_list, archived_directories_filtered_by_jd = [], []
+
+    # Iterate through the list of archived directories newest first
+    # appending to the list of directories to be considered
     for directory in archived_directories:
         archived_directories_filtered_by_jd.append(directory)
+        # As soon as a directory has been added which is before the latest_jd
+        # stop appending break the loop; everything else has already been processed
         if rmsTimeExtractor(directory, asJD=True) < latest_jd:
             print("Excluding directories before {}, already processed for {}".format(
                                 os.path.basename(directory), config.stationID))
             break
+
+    # Reverse the list again, so that the oldest is at the top.
     archived_directories_filtered_by_jd.reverse()
+
+    # Working with each of the remaining archived directories write into the database
     for dir in archived_directories_filtered_by_jd:
-        print("Working on {}".format(dir))
+
+        # Get full paths to critical files
         full_path = os.path.join(archived_directories_path, dir)
         full_path_calstars = glob.glob(os.path.join(full_path,"*CALSTARS*" ))
         full_path_platepar = glob.glob(os.path.join(full_path, "platepar_cmn2010.cal"))
+
+        # If no platepar is found or no calstars, then ignore this directory
         if len(full_path_platepar) != 1 or len(full_path_calstars) != 1:
             continue
+
         full_path_platepar, full_path_calstars = full_path_platepar[0], full_path_calstars[0]
-        print(full_path_calstars)
-        print(full_path_platepar)
         calstars_path = os.path.dirname(full_path_calstars)
         calstars_name = os.path.basename(full_path_calstars)
+
+        # Read in the CALSTARS file
         calstar = readCALSTARS(calstars_path, calstars_name)
 
-
-
-        calstar_list.append(convertRaDec(calstar, conn, catalogue, full_path, latest_jd))
+        # Put the CALSTARS list into the database
+        calstar_list.append(calstarToDb(calstar, conn, full_path, latest_jd))
 
 
 
 def getCatalogueID(r, d, conn, margin=0.3):
+    """
+    Get the local for the brightest star within margin degrees of passed radec
+    Args:
+        r ():  right ascension (degreees)
+        d (): declination (degrees)
+        conn (): database connection
+        margin (): optional, default 0.3, degrees margin. This is not a skyarea, simply a box in the
+                    interest of computational efficiency
+
+    Returns:
+        tuple (id, magnitude, catalogue right ascension, catalogue declination)
+    """
 
     sql_command = ""
     sql_command += "SELECT id, mag, r, d FROM catalogue \n"
@@ -147,15 +177,31 @@ def getCatalogueID(r, d, conn, margin=0.3):
         return 0, 0, 0, 0
 
 
-def photometry(config, pp_all, calstar, match_radius = 2.0):
+def computePhotometry(config, pp_all, calstar, match_radius=2.0, star_margin = 1.2):
 
+    """
+    Compute photometric offset and vignetting coefficient from CALSTARS
+    Best practice is to to use the vignetting coeffificnt from the platepar
+    not a computed number
 
+    Args:
+        config (): configuration instance
+        pp_all (): a dictionary of all recomputed platepars
+        calstar (): calstar data structure
+        match_radius (): the pixel radius used by the recalibration routine
+
+    Returns:
+        tuple(photometric offset, vignetting coefficient)
+    """
+
+    # Extract stars from the catalogue one order of magnitude dimmer than config limit
     lim_mag = config.catalog_mag_limit + 1
-
     catalog_stars, mag_band_str, config.star_catalog_band_ratios = readStarCatalog(config.star_catalog_path,
                                             config.star_catalog_file, lim_mag=lim_mag,
                                                 mag_band_ratios=config.star_catalog_band_ratios)
 
+    # star_dict contains the star data from calstars - indexed by jd
+    # ff_name contains the fits file name - indexed by jd
     star_dict, ff_dict = {}, {}
     max_stars = 0
     ff_most_stars = None
@@ -169,98 +215,150 @@ def photometry(config, pp_all, calstar, match_radius = 2.0):
             if ff_name in pp_all:
                 max_stars, ff_most_stars, jd_most = star_count, ff_name, jd
 
+    # As the purpose of this code is to get the best magnitude information, discard observation sessions where
+    # too few stars were observed, returning none from here will discard the whole observation session
     pp = Platepar()
-    if ff_most_stars is None or max_stars < config.min_matched_stars:
+    if ff_most_stars is None or max_stars < config.min_matched_stars * star_margin:
         print("Too few stars, moving on")
         return None, None
 
-
+    # Build a list of matched stars for photometry computations
     pp.loadFromDict(pp_all[ff_most_stars])
     n_matched, avg_dist, cost, matched_stars = matchStarsResiduals(config, pp, catalog_stars,
                                         {jd_most: star_dict[jd_most]}, match_radius, ret_nmatch=True,
                                                                    lim_mag=lim_mag)
 
+    # If jd_most is not in matched stars, then do not use this observation session.
+    # This is probably caused by too few stars
+
     if jd_most not in matched_stars:
         print("Key error, moving on")
         return None, None
 
-    print("jd with most stars {} as date {}".format(jd_most, jd2Date(jd, dt_obj=True)))
+    # Split the data return from matched_stars into image stars and catalogue stars
     image_stars, matched_catalog_stars, distances = matched_stars[jd_most]
+
+    # Get the star intensities
     star_intensities = image_stars[:, 2]
+
+    # Transpose the matched_catalog_stars array and extract ra, dec, mag
     cat_ra, cat_dec, cat_mags = matched_catalog_stars.T
+
+    # For every star on the image compute the radius from image centre
     radius_arr = np.hypot(image_stars[:, 0] - pp.Y_res / 2, image_stars[:, 1] - pp.X_res / 2)
+
+    # Correct for extinction
     mag_cat = extinctionCorrectionTrueToApparent(cat_mags, cat_ra, cat_dec, jd, pp)
 
+    # Conduct the photometry fit, probably should do something with the standard deviation
     photom_params, fit_stddev, fit_resid, star_intensities, radius_arr, catalog_mags = \
         photometryFitRobust(star_intensities, radius_arr, mag_cat)
 
     return photom_params
 
-def convertRaDec(calstar, conn, catalogue, archived_directories_path, latest_jd=0):
+def calstarToDb(calstar, conn, archived_directory_path, latest_jd=0):
 
     """
-    Parses a calstar data structure, retains all existing data but
-    uses the
+    Parses a calstar data structures in archived directories path,
+    converts to RaDec, corrects magnitude data and writes newer data to database
 
     Args:
-        calstar (): a calstar data structure
-
+        calstar (): calstar data structure for one observation session
+        conn (): connection to database
+        archived_directory_path ():
+        latest_jd (): optional, default 0, latest jd for this station in the database
 
     Returns:
-
+        calstar_radec (): list of stellar magnitude data in radec format
     """
 
+    # Intialise calstar_radec list
     calstar_radec = []
 
-    platepars_all_recalibrated_path = os.path.join(archived_directories_path, "platepars_all_recalibrated.json")
+    # Get the path to all the recalibrated platepars for the night and read them in
+    platepars_all_recalibrated_path = os.path.join(archived_directory_path, "platepars_all_recalibrated.json")
     with open(platepars_all_recalibrated_path, 'r') as fh:
         pp_recal = json.load(fh)
 
+    # Compute photometry offset and vignetting using the best data from the night
+    # vignetting coefficient will be overwritten by platepar value
     offset, vignetting = photometry(config, pp_recal, calstar)
+
+    # If this can't be computed, then probably the night was a poor observation session, so reject all
     if offset is None or vignetting is None:
-        print("Nothing found in {}, moving on".format(archived_directories_path))
+        print("Nothing found in {}, moving on".format(archived_directory_path))
         return
+
+    # Iterate through the calstar data structure for each image in the whole night
     for fits_file, star_list in tqdm.tqdm(calstar):
+
+        # If too few stars on this specific observation, then ignore
         if len(star_list) < config.min_matched_stars:
             continue
+
+        # Get the data and time of this observation
         date_time, jd = rmsTimeExtractor(fits_file, asTuple=True)
         # Skip anything which has already been processed
         if jd < latest_jd:
             continue
+
+        # If this fits_file does not have a recalibrated platepar, then skip
         if not fits_file in pp_recal:
             continue
+
+        # If it does, then load the recalibrated platepar for this image
         pp = Platepar()
         pp.loadFromDict(pp_recal[fits_file])
 
+        # Overwrite vignetting coefficient with platepar value
+        vignetting = pp.vignetting_coeff
         jd_list, y_list, x_list, bg_list, amp_list, FWHM_list = [], [], [], [], [], []
-        for y, x, bg_intensity, amplitude, FWHM in star_list:
 
+        # Build up lists of data for this image
+        for y, x, bg_intensity, amplitude, FWHM in star_list:
             jd_list.append(jd)
             x_list.append(x)
             y_list.append(y)
             bg_list.append(bg_intensity)
             amp_list.append(amplitude)
             FWHM_list.append(FWHM)
+
+        # Convert to arrays
         jd_arr, x_data, y_data, level_data = np.array(jd_list), np.array(x_list), np.array(y_list), np.array(amp_list)
 
-        jd, ra, dec, mag = xyToRaDecPP(jd_arr, x_data, y_data, level_data, pp, jd_time=True, extinction_correction=False, measurement=True)
+        # Process data into RaDec and apply magnitude corrections
+        jd, ra, dec, mag = xyToRaDecPP(jd_arr, x_data, y_data, level_data, pp,
+                                                jd_time=True, extinction_correction=False, measurement=True)
+
         star_list_radec = []
         for j, x, y, r, d, bg, amp, FWHM, mag in zip(jd, x_list, y_list, ra, dec, bg_list, amp_list, FWHM_list, mag):
             cat_id, cat_mag, cat_r, cat_d = getCatalogueID(r, d, conn)
             az, el = raDec2AltAz(r, d, j, pp.lat, pp.lon)
             radius = np.hypot(y - pp.Y_res / 2, x - pp.X_res / 2)
-            mag = 0 - 2.5 * np.log10(correctVignetting(amp, radius, vignetting)) + offset
+            mag = 0 - 2.5 * np.log10(correctVignetting(amp, radius, pp.vignetting_coeff)) + offset
             if mag == np.inf:
                 continue
             star_list_radec.append([j, date_time, fits_file, x, y, az, el, r, d, bg, amp,
                                                                     FWHM, mag, cat_id, cat_mag, cat_r, cat_d])
+        # Check that we still have enough stars and write to database
         if len(star_list_radec) > config.min_matched_stars:
             insertDB(config, conn, star_list_radec)
+
+        # Add the data to the calstar_radec list
         calstar_radec.append([fits_file, star_list_radec])
     return calstar_radec
 
 def insertDB(config, conn, star_list_radec):
+    """
+    Write data into the stellar magnitudes database
+    Args:
+        config (): config instance
+        conn (): database connection
+        star_list_radec (): star_list in radec format with corrected magnitudes
 
+    Returns:
+
+    """
 
     for jd, date_time, fits, x, y, az, el, r, d,  bg, amp, FWHM, mag, cat_id, cat_mag, cat_r, cat_d in star_list_radec:
         sql_command = ""
@@ -274,17 +372,15 @@ def insertDB(config, conn, star_list_radec):
     conn.commit()
 
 def getStationStarDBConn(db_path, force_delete=False):
-    """ Creates the station star database. Tries only once.
-
-    arguments:
-        config: config file
-        force_delete: if set then deletes the database before recreating
-
-    returns:
-        conn: [connection] connection to database if success else None
-
     """
+    Get the connection to the stellar magnitude database, if it does not exist, then create
+    Args:
+        db_path (): full path to database
+        force_delete (): optional, default false, delete and create
 
+    Returns:
+        conn (): connection object instance
+    """
     # Create the station star database
 
     if force_delete:
@@ -307,7 +403,7 @@ def getStationStarDBConn(db_path, force_delete=False):
 def retrieveMagnitudesAroundRaDec(conn, r,d, window=0.5, start_time=None, end_time=None):
 
     """
-
+    Query the database on conn to find magnitudes around r, d. This might return more than one star
     Args:
         r (): right ascension in degrees
         d (): declination in degrees
@@ -316,7 +412,7 @@ def retrieveMagnitudesAroundRaDec(conn, r,d, window=0.5, start_time=None, end_ti
         end_time (): jd of end
 
     Returns:
-
+        list of tuples (jd, stationID, r, d, mag, cat_mag)
     """
     window = abs(window)
     sql_command = ""
@@ -332,6 +428,14 @@ def retrieveMagnitudesAroundRaDec(conn, r,d, window=0.5, start_time=None, end_ti
 
 def createTableStarObservations(conn):
 
+    """
+    If the star_observations table does not exist, then create
+    Args:
+        conn (): connection to database
+
+    Returns:
+
+    """
     table_name = "star_observations"
     # Returns true if the table exists in the database
     try:
@@ -377,6 +481,14 @@ def createTableStarObservations(conn):
 
 def createTableCatalogue(conn):
 
+    """
+    Creates the catalogue table if it does not exist
+    Args:
+        conn (): connection to database
+
+    Returns:
+        connection to database
+    """
     table_name = "catalogue"
     # Returns true if the table exists in the database
     try:
@@ -406,6 +518,17 @@ def createTableCatalogue(conn):
 
 def findMostRecentEntry(config, conn):
 
+    """
+    Get the most recent entry for the station id in config object
+    in the stellar magnitude database
+
+    Args:
+        config (): config instance
+        conn (): connection instance
+
+    Returns:
+        jd of most recent entry for this station
+    """
 
     sql_command = ""
     sql_command += "SELECT max(jd) FROM star_observations \n"
@@ -421,6 +544,7 @@ def findMostRecentEntry(config, conn):
 
 def loadGaiaCatalog(dir_path, file_name, lim_mag=None):
     """ Read star data from the GAIA catalog in the .npy format.
+        This function copied here to avoid reading in whole of SkyFit2
 
     Arguments:
         dir_path: [str] Path to the directory where the catalog file is located.
@@ -448,7 +572,14 @@ def loadGaiaCatalog(dir_path, file_name, lim_mag=None):
     return results
 
 def catalogueToDB(conn):
+    """
+    Read catalogue into database
+    Args:
+        conn (): connection instance
 
+    Returns:
+        Nothing
+    """
     catalogue = loadGaiaCatalog("~/source/RMS/Catalogs", "gaia_dr2_mag_11.5.npy", lim_mag=11)
     for star in tqdm.tqdm(catalogue):
         sql_command = "INSERT INTO catalogue (r , d, mag) \n"
@@ -456,11 +587,21 @@ def catalogueToDB(conn):
         conn.execute(sql_command)
     conn.commit()
 
-def createPlot(values, r, d, w):
+def createPlot(values, r, d, w=0):
+    """
 
+    Args:
+        values (): list of values to be plotted as (jd, stationID, ra, dec, mag, cat_mag)
+        r (): right ascension, used only for title
+        d (): declination, used only for title
+        w (): window, used only for title
+
+    Returns:
+
+    """
 
     x_vals, y_vals = [], []
-    title = "Plot of magnitudes at RA {} Dec {}".format(r,d)
+    title = "Plot of magnitudes at RA {} Dec {}, window {}".format(r,d, w)
     for jd, stationID, r, d, mag, cat_mag in values:
         x_vals.append(jd)
         y_vals.append(mag)
