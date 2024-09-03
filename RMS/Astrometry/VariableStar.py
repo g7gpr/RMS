7efd745d29984c6d7b1ef469975abcf91755514b
David Rollinson <g7gpr@outlook.com>
2024-09-03 13:13:11 +0000

    Prevent iterating beyond end of array

--- RMS/Astrometry/VariableStar.py
+++ RMS/Astrometry/VariableStar.py
@@ -878,6 +878,8 @@ def assembleContactSheet(thumbnail_list, x_across=None, border = 1):
         tn = 0
         headings_list, position_list = [], []
         for y in range(0, down, y_res):
+            if tn > thumbnail_count:
+                break
             fits, _ = thumbnail_list[tn]
             headings_list.append(rmsTimeExtractor(fits).strftime("%Y%m%d_%H%M%S"))
             position_list.append(y)
