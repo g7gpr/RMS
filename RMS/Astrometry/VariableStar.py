09f641f5721a6887358dd447e8d7df607005b0e8
David Rollinson <g7gpr@outlook.com>
2024-09-04 01:12:47 +0000

    Show progress

--- RMS/Astrometry/VariableStar.py
+++ RMS/Astrometry/VariableStar.py
@@ -526,8 +526,8 @@ def calstarToDb(calstar, conn, archived_directory_path, latest_jd=0):
         return
 
     # Iterate through the calstar data structure for each image in the whole night
-
-    for fits_file, star_list in calstar:
+    print("Iterating through calstar list for {}".format(rmsTimeExtractor(archived_directory_path)))
+    for fits_file, star_list in tqdm.tqdm(calstar):
 
         # If too few stars on this specific observation, then ignore
         if len(star_list) < config.min_matched_stars:
