573b23ee67b54a1f49fba674b84a797687f42ce9
David Rollinson <g7gpr@outlook.com>
2024-09-07 16:24:02 +0000

    Do not copy default station

--- Utils/MultiCam/AddPi5Station.py
+++ Utils/MultiCam/AddPi5Station.py
@@ -321,6 +321,11 @@ def copyPiStation(config_path ="~/source/RMS/.config", first_station = False, ne
     # Get the path to the config file
     config_path = os.path.expanduser(config_path)
 
+    # Do not copy the default station
+
+    if "XX" in config.stationID:
+        return
+
     # Check the config path exists and the use to create stations
     if os.path.exists(config_path):
         config = cr.parse(config_path)
