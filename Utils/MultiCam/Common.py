da90418c4bd5a03691394c9150b16f54ad573f9d
David Rollinson <g7gpr@outlook.com>
2024-09-07 14:39:44 +0000

    Get directory name if whole path was passed

--- Utils/MultiCam/Common.py
+++ Utils/MultiCam/Common.py
@@ -414,6 +414,8 @@ def setQuotas(path_to_config, quotas_tuple, debug=False):
 		print("             arch_dir_quota {}".format(quotas_tuple[1]))
 		print("            bz2_files_quota {}".format(quotas_tuple[2]))
 
+	if os.path.isfile(path_to_config):
+		path_to_config = os.path.dirname(path_to_config)
 
 	fh = open(os.path.join(path_to_config,".config"),"r")
 	config_lines = []
