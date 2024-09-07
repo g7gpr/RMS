10473762c45fab6989fbd9b8f6d64f5da70eb66b
David Rollinson <g7gpr@outlook.com>
2024-09-07 11:03:34 +0000

    Experimenting with direct launch

--- Utils/MultiCam/AddPi5Station.py
+++ Utils/MultiCam/AddPi5Station.py
@@ -472,6 +472,7 @@ if __name__ == "__main__":
     for entry in station_list:
         print("Starting {}".format(entry))
         path_to_config = os.path.expanduser(os.path.join("~/source/Stations/",entry.lower()))
-        launch_command = "source /home/{}/vRMS/bin/activate; python -m RMS.StartCapture -c {}".format("rms",path_to_config)
-        proc = subprocess.run(["/usr/bin/lxterminal -e '{}'".format(launch_command)])
+        launch_command = "/usr/bin/lxterminal -e "
+        launch_command += "'source /home/{}/vRMS/bin/activate; python -m RMS.StartCapture -c {}'".format("rms",path_to_config)
+        proc = subprocess.run([launch_command])
         time.sleep(60)
