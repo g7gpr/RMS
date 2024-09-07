eac58906355fb7170396630da1d7e73608e18249
David Rollinson <g7gpr@outlook.com>
2024-09-07 10:54:37 +0000

    Delay station starts

--- Utils/MultiCam/AddPi5Station.py
+++ Utils/MultiCam/AddPi5Station.py
@@ -472,3 +472,4 @@ if __name__ == "__main__":
         path_to_config = os.path.expanduser(os.path.join("~/source/Stations/",entry.lower()))
         launch_command = "source ~/vRMS/bin/activate; python -m RMS.StartCapture -c {}".format(path_to_config)
         proc = subprocess.run(["{}lxterminal -e '{}'".format("/usr/bin/",launch_command)])
+        time.sleep(60)
