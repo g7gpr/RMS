7511829a42ac8de044c4035f60cd69b78229e57c
David Rollinson <g7gpr@outlook.com>
2024-09-07 10:22:04 +0000

    Initial commit of multicam in python

---
+++ Utils/MultiCam/AddGnomeStation.py
@@ -0,0 +1,357 @@
+# This software is part of the Linux port of RMS
+# Copyright (C) 2023  Ed Harman
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from RMS.Misc import isRaspberryPi
+import os
+import subprocess
+import argparse
+from datetime import datetime
+from RMS.Misc import mkdirP
+from shutil import copy
+from Utils.MultiCam.Common import createAutoStartEntry, createShowLiveStreamEntry
+from Utils.MultiCam.Common import checkUserDesktopDirectoryEnvironment, customiseConfig, makeKeys
+
+
+def detectPi():
+
+	"""
+	Discover if this is Raspberry Pi platform, and terminate if it is
+
+	Returns:
+		nothing
+	"""
+
+	if isRaspberryPi():
+		print("The add_GStation.sh script should not be used on Raspberry Pi.")
+		print("Please use add_Pi_Station.sh to add cameras on a Pi5.")
+		exit(1)
+
+
+def install(package, purge_first = False, ask = False):
+	"""
+	Call apt-get to install a package
+	Args:
+		package (): package to be installed
+		purge_first (): optional, default false, remove the package before installation
+		ask (): option, default false, ask terminal before installing
+
+	Returns:
+		Nothing
+	"""
+
+
+	if purge_first:
+		subprocess.run(['sudo', '-S', 'apt-get', 'purge', package, "-y"])
+
+	if subprocess.run(['dpkg', '-L', package], capture_output=True).returncode != 0:
+		if ask:
+			response = input("Do you wish to install {}?".format(package)).lower()
+			if response == 'y':
+				do_install = True
+			else:
+				do_install = False
+		else:
+			do_install = True
+	else:
+		do_install = False
+	if do_install:
+		subprocess.run(['sudo', '-S', 'apt-get', 'install', package, "-y"])
+
+	else:
+		pass
+
+def getLxSettings():
+
+	"""
+	Function to return settings to be applied to the LX Terminal settings file
+	Returns:
+		list of settings to be applied
+	"""
+
+
+	settings = []
+	settings.append(["scrollback","10000"])
+	settings.append(["geometry_columns", "120"])
+	settings.append(["geometry_rows", "25"])
+
+	return settings
+
+
+
+
+def configureLxTerminal(settings_list, config_file_path_and_name):
+
+	"""
+	Edit the LX Terminal settings file
+	Args:
+		settings_list : list of settings
+		config_file_path_and_name : path and name to config file
+
+	Returns:
+		nothing
+	"""
+
+
+	subprocess.run(['lxterminal','-e','exit'])
+	config_file_path_and_name = os.path.expanduser(config_file_path_and_name)
+
+	fh = open(config_file_path_and_name, "r")
+	config_as_list = []
+	for line in fh:
+		config_as_list.append(line)
+	fh.close()
+
+	output_line_list = []
+	for line in config_as_list:
+		written_line = False
+		for setting, value in settings_list:
+			if line[:len(setting)] == setting:
+				output_line_list.append ("{}={}\n".format(setting,value))
+				written_line = True
+		if not written_line:
+			output_line_list.append(line)
+
+	fh = open(config_file_path_and_name, "w")
+	fh.writelines(output_line_list)
+	fh.close()
+
+def getStationsToAdd():
+
+	"""
+	Request user input for which stations to add.
+	Not compatible with Python 2.7 as input behaves very differently.
+	Returns:
+		list of stations to add
+	"""
+
+	stations_list = []
+
+	while True:
+		response = input("Enter station ID, <cr> to end: ")
+		if response == "":
+			break
+		else:
+			stations_list.append(response)
+
+
+	return stations_list
+
+
+def addStations(stations_list):
+
+	"""
+	Create and populate the folder structure for the new stations
+
+	In brief this is
+
+	~/source/Stations/xx0001/
+					 /xx0002/
+					 /xx0003/
+
+
+	Args:
+		stations_list (): a list of station to be added
+
+	Returns:
+		nothing
+	"""
+
+	source_dir = os.path.expanduser("~/source")
+	stations_path = os.path.join(source_dir, "Stations")
+
+	mkdirP(stations_path)
+	mkdirP(os.path.expanduser("~/RMS_data"))
+	mkdirP(os.path.expanduser("~/.config/autostart"))
+
+	for station in stations_list:
+		station = station.lower()
+		new_station_config_path = os.path.join(stations_path, station)
+		if os.path.exists(new_station_config_path):
+			print("Station {} already exists".format(station.upper()))
+		else:
+			addStation(station, len(stations_list))
+
+
+def createSoftLinksForStartCapture(station, free_space_allocation_per_station = 30):
+
+	"""
+	Create the softlinks required for a single station
+
+	Args:
+		station (): stationID
+		free_space_allocation_per_station (float): optional default 30 GB free space to be allocated per station
+
+	Returns:
+		nothing
+	"""
+
+	src_path = os.path.expanduser("~/.config/autostart/{}_StartCap.desktop".format(station))
+	dest_path = os.path.expanduser("~/Desktop/{}_StartCap.desktop".format(station))
+	os.chmod(src_path, 0o755)
+	if os.path.exists(dest_path):
+		os.unlink(dest_path)
+	os.symlink(src_path, dest_path)
+	os.chmod(dest_path, 0o755)
+	subprocess.run(['gio', 'set', src_path,"metadata::trusted", "true"])
+
+
+def addStation(station, number_of_stations):
+
+	"""
+	Add the station, and customise the .config file as required
+
+	Args:
+		station (): stationID
+		number_of_stations (): total_number_of_stations, used for configuring extra_space parameter
+
+	Returns:
+
+	"""
+
+	# Create path variables
+	source_dir = os.path.expanduser("~/source")
+	stations_path = os.path.join(source_dir, "Stations")
+	original_config_path = os.path.join(source_dir, "RMS", ".config")
+	new_station_config_path = os.path.join(stations_path, station.lower())
+	station_config_path_and_name = os.path.join(new_station_config_path, ".config")
+
+	# Create the new directory for station config, mask, platepar etc
+	mkdirP(new_station_config_path)
+	copy(original_config_path, station_config_path_and_name)
+
+	# Setup the start capture path
+	desktop_start_capture_path = os.path.expanduser("~/.config/autostart/{}_StartCap.desktop".format(station))
+	if os.path.exists(desktop_start_capture_path):
+		os.unlink(desktop_start_capture_path)
+
+	# Setup the auto start for this station
+	fh = open(desktop_start_capture_path,"w")
+	fh.writelines(createAutoStartEntry(station))
+	fh.close()
+	os.chmod(desktop_start_capture_path, 0o755)
+	createSoftLinksForStartCapture(station)
+
+	# Move the mask to the new location
+	copy(os.path.join(source_dir,"RMS/mask.bmp"),new_station_config_path)
+
+	# Create the path for the live_stream icon
+	desktop_show_live_stream_path = os.path.expanduser("~/Desktop/Show_LiveStream-{}.desktop".format(station))
+	if os.path.exists(desktop_show_live_stream_path):
+		os.unlink(desktop_show_live_stream_path)
+
+	# Create the icon
+	fh = open(desktop_show_live_stream_path,"w")
+	fh.writelines(createShowLiveStreamEntry(station))
+	fh.close()
+	os.chmod(desktop_show_live_stream_path, 0o644)
+
+	subprocess.run(['gio', 'set', desktop_show_live_stream_path, "metadata::trusted", "true"])
+	os.chmod(desktop_show_live_stream_path, 0o777)
+	data_dir = os.path.join(os.path.expanduser("~/RMS_data"),station)
+	extra_space = number_of_stations * free_space_allocation_per_station
+
+	# Apply the new settings the .config file
+	customiseConfig(new_station_config_path, station.upper(), data_dir, extra_space, reboot_after_processing=False)
+
+
+def applyGsettings(screen_timeout = 600, lock_screen_enabled = False):
+
+	"""
+	Apply appropriate settings to Gnome
+	Args:
+		screen_timeout (): optional, default 600, screen lock time_out
+		lock_screen_enabled (): optional, default false, screen_lock
+
+	Returns:
+		nothing
+	"""
+
+	if lock_screen_enabled:
+		lock_screen_setting = "true"
+	else:
+		lock_screen_setting = "false"
+	subprocess.run(['gsettings', 'set', 'org.gnome.settings-daemon.plugins.power', 'sleep-inactive-ac-type', "'nothing'"])
+	subprocess.run(['gsettings', 'set', 'org.gnome.settings-daemon.plugins.power', 'sleep-inactive-ac-timout', '0'])
+	subprocess.run(['gsettings', 'set', 'org.gnome.desktop.session', 'idle-delay', str(screen_timeout)])
+	subprocess.run(['gsettings', 'set', 'org.gnome.desktop.screensaver', 'lock_enabled', lock_screen_setting])
+
+
+def addActivate(bashrc_path=os.path.expanduser("~/.bashrc")):
+
+	"""
+	Update bashrc with settings for RMS
+	Args:
+		bashrc_path (): optional, default ~/.bashrc, path to the .bashrc file
+
+	Returns:
+		nothing
+	"""
+
+
+	bashrc_path = os.path.expanduser("~/.bashrc")
+
+	fh = open(bashrc_path,'r')
+
+	for line in fh:
+		if "cd ~/source/RMS" in line:
+			return
+	fh.close()
+
+	fh = open(bashrc_path, 'a')
+	fh.write("\n")
+	fh.write("# added by RMS {}\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+	fh.write("\n")
+	fh.write("cd ~/source/RMS\n")
+	fh.write("source ~/vRMS/bin/activate")
+	fh.close()
+
+
+
+if __name__ == "__main__":
+
+	arg_parser = argparse.ArgumentParser(description="Add a station to MultiCameraLinux.")
+
+	arg_parser.add_argument("-d","--rms_data", nargs='?', default="~/RMS_data", help="Path to the data directory")
+
+	arg_parser.add_argument("-s","--stations", default="",
+							help="Stations to add")
+
+
+
+	# Parse the args
+	cml_args = arg_parser.parse_args()
+
+	# Are we running on Pi, should not be - will exit if we are
+	detectPi()
+	# I am not sure what to do with this information?
+	checkUserDesktopDirectoryEnvironment()
+
+	install("lxterminal", purge_first=False)
+	configureLxTerminal(getLxSettings(), "~/.config/lxterminal/lxterminal.conf" )
+
+	if cml_args.stations == "":
+		stations_list = getStationsToAdd()
+	else:
+		stations_list = cml_args.stations.split(",")
+
+	addStations(stations_list)
+	makeKeys()
+	setTimeZone()
+	applyGsettings()
+	addActivate()
+	install("pcmanfm", ask=True)
+	install("mousepad", ask=True)
---
+++ Utils/MultiCam/AddPi5Station.py
@@ -0,0 +1,466 @@
+# This software is part of the Linux port of RMS
+# Copyright (C) 2023  Ed Harman
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This version history no longer maintained following migration into git
+#
+# Version 1.7   - changed desktop launcher display names and added conkyrc1 mod
+#
+# Version 1.6   - added support for RPi-5 platforms that can run max 4 cameras
+#
+# Version 1.5   - added support for non English locales where user user directories may not include a directory named Desktop
+#		 i.e. this enables support of RMS on a non English distro install
+#
+# Version 1.4   - moved codebase into RMS/Scripts/MultiCamLinux
+#
+# Version 1.3	- fixed path to CMN desktop shortcut
+#
+# Version 1.2	- added a  change to the flag reboot_after_processing from true to false
+#
+# Version 1.1
+# Changes	- added station arguments to  Launch scripts
+#		- changed desktop links  for StartCapture to symbolic links of the scripts within .config/autostart
+#
+# Version 1.2	- Peter E. took over dev of this script and any blame going forward
+#		- white space and indents added
+#
+# Translated to Python and revision history managed in git
+
+import subprocess
+import os
+import urllib.request
+
+import RMS.ConfigReader as cr
+from RMS.Misc import sanitise, mkdirP
+from Utils.MultiCam.Common import createAutoStartEntry, createShowLiveStreamEntry, setTimeZone, setQuotas
+from Utils.MultiCam.Common import checkUserDesktopDirectoryEnvironment, uncomment, computeQuotas
+from Utils.MultiCam.Common import moveIfExists, copyIfExists, changeOptionValue
+from Utils.MultiCam.Common import getStationsToAdd, customiseConfig, makeKeys
+import argparse
+
+MAX_STATION = 4	# maximum number of stations allowed on a Pi5
+
+def lsCPU():
+
+    """
+
+    Returns: [str] CPU information
+
+    """
+
+    return subprocess.check_output("lscpu").decode('ascii')
+
+
+def piModel(read_from = "/proc/cpuinfo"):
+    """
+
+    Args:
+        read_from: [str] optional, default "/proc/cpuinfo".
+
+    Returns:
+        Model of Pi
+    """
+
+
+    if os.path.exists(read_from):
+        with open(read_from, 'r') as f:
+            line_list = list(filter(lambda x: 'Model' in x, f.readlines()))
+
+            if len(line_list):
+                model_line_list = line_list[0].split(":")
+            else:
+                return None
+
+            if len(model_line_list) == 2:
+                return sanitise((model_line_list[1]), space_substitution=" ").strip()
+            else:
+                return None
+
+    else:
+        return "Unable to determine"
+
+def isPi5():
+    """
+
+    Returns:[bool] True if hardware is identifed as Pi5
+
+    """
+
+    if piModel() is None:
+        return False
+    else:
+        return "Pi 5" in piModel()
+
+def firstStationConfigured(config_path = "~/source/RMS/.config"):
+    """
+    Examines the .config file in RMS path to discover if station is still set as XX0001
+    This is a good indicator that no configuration work has been carried out
+    Args:
+        config_path: [path] optional default ~/source/RMS/.config
+
+    Returns:[bool] True if stationID is no longer XX0001
+
+    """
+    config_path = os.path.expanduser(config_path)
+    if os.path.exists(config_path):
+        config = cr.parse(config_path)
+    else:
+        return False
+    if config.stationID == "XX0001":
+        return False
+    else:
+        return True
+
+def computeNewStationPaths(config, new_station_id = None, stations_folder = "Stations"):
+    """
+
+    Args:
+        config: [config] Config instance
+        new_station_id: [str] New station id e.g. AU0004
+        stations_folder: [str] optional, default Stations, name of the folder in ~/RMS to be created
+
+    Returns:
+        [tuple](path,path) new_station_config_location, new_station_data_dir
+    """
+
+    existing_station_config_path = os.path.expanduser("~/source/RMS/.config")
+    if new_station_id is None:
+        new_station_id = config.stationID
+
+    new_station_id =new_station_id.lower()
+
+    if os.path.exists(existing_station_config_path):
+        stations_config_directory = os.path.dirname(os.path.expanduser("~/source/RMS"))
+        new_station_config_location = os.path.join(stations_config_directory, stations_folder, new_station_id)
+        new_station_data_dir = os.path.join(config.data_dir, new_station_id)
+
+    return new_station_config_location, new_station_data_dir
+
+def configureFirstStation(path_to_config = "~/source/RMS/.config"):
+    """
+    Gets operator input to configure the first station.
+    Latitude, longitude and elevation will be propagated to all other cameras configured for this station
+    station id and ip address will only be used for the first station to be configured
+
+    Args:
+        path_to_config: [path] optional, default "~/source/RMS/.config
+    Returns:
+        Nothing
+    """
+    path_to_config = os.path.expanduser(path_to_config)
+    print("First station not yet configured")
+    print("Please provide a stationID, location of the station, and the camera IP address")
+    print("These values can be edited later")
+    station_id = input("first station id:").upper()
+    latitude = input("latitude (wgs84):")
+    longitude = input("latitude (wgs84):")
+    elevation = input("Elevation in metres above mean sea level:")
+    ip_address = input("first station camera ip address:")
+
+    if os.path.exists(path_to_config):
+        pass
+    else:
+        getConfigFromMaster(dest_path=os.path.dirname(path_to_config))
+
+
+    fh = open(path_to_config, "r")
+    config_lines = []
+    for line in fh:
+        config_lines.append(line)
+    fh.close()
+
+    config_lines = changeOptionValue(config_lines, "stationID", station_id)
+    config_lines = changeOptionValue(config_lines, "latitude", latitude)
+    config_lines = changeOptionValue(config_lines, "longitude", longitude)
+    config_lines = changeOptionValue(config_lines, "altitude", elevation)
+    config_lines = changeOptionValue(config_lines, "ip_address", ip_address)
+
+    fh = open(path_to_config, "w")
+    fh.writelines(config_lines)
+    fh.close()
+
+
+def getMaskFromMaster(source_url = "https://raw.githubusercontent.com/CroatianMeteorNetwork/RMS/prerelease/mask.bmp"
+                                                                                        , dest_path = "~/source/RMS"):
+    """
+    Download mask file from github
+    Args:
+        source_url: [str] source URL for .config file
+        dest_path: [path] destination for .config ilfe
+
+    Returns: [bool] True if mask download succeeded
+
+    """
+
+    dest_path = os.path.expanduser(os.path.join(dest_path, os.path.basename(source_url)))
+    mkdirP(os.path.dirname(dest_path))
+    urllib.request.urlretrieve(source_url, dest_path)
+
+    if os.path.exists(dest_path):
+        print("Successful download of mask from github")
+        return True
+    else:
+        print("Mask download from github failed")
+        return False
+
+def getConfigFromMaster(source_url = "https://raw.githubusercontent.com/CroatianMeteorNetwork/RMS/prerelease`/.config"
+                                                                                        , dest_path = "~/source/RMS"):
+
+    """
+    Get the .config file from github
+    Args:
+        source_url: [str] URL to download config from
+        dest_path: [path] location to save the .config file
+
+    Returns: [bool] True if succeeded
+
+    """
+
+    dest_path = os.path.expanduser(os.path.join(dest_path, os.path.basename(source_url)))
+
+    urllib.request.urlretrieve(source_url, dest_path)
+
+    if os.path.exists(dest_path):
+        print("Successful download of .config from github")
+        return True
+    else:
+        print(".config download from github failed")
+        return False
+
+
+
+def createDesktopShortcuts(station):
+    """
+    Create the desktop shortcuts
+    Args:
+        station: [str] stationID
+
+    Returns:
+        Nothing
+    """
+
+    desktop_start_capture_path = os.path.expanduser("~/Desktop/{}_StartCapture.sh".format(station))
+    if os.path.exists(desktop_start_capture_path):
+        os.unlink(desktop_start_capture_path)
+    fh = open(desktop_start_capture_path, "w")
+    fh.writelines(createAutoStartEntry(station))
+    fh.close
+
+    desktop_show_live_stream_path = os.path.expanduser("~/Desktop/Show_LiveStream-{}.desktop".format(station))
+    if os.path.exists(desktop_show_live_stream_path):
+        os.unlink(desktop_show_live_stream_path)
+
+    fh = open(desktop_show_live_stream_path, "w")
+    fh.writelines(createShowLiveStreamEntry(station))
+    fh.close()
+    os.chmod(desktop_show_live_stream_path, 0o644)
+
+def cleanDesktop():
+    """
+        Remove disused files from Desktop
+    Returns:
+        Nothing
+    """
+
+    files_to_delete = ["~/Desktop/CMNbinViewer.sh","~/Desktop/RMS_ShowLiveStream.sh","~/Desktop/RMS_StartCapture.sh",
+	                   "~/Desktop/RMS_config.txt", "~/Desktop/TunnelIPCamera.sh", "~/Desktop/DownloadOpenVPNconfig.sh"]
+
+    for f in files_to_delete:
+        full_path = os.path.expanduser(f)
+        if os.path.exists(full_path):
+            os.unlink(full_path)
+
+
+
+def copyPiStation(config_path ="~/source/RMS/.config", first_station = False, new_station_id = None, debug=False):
+
+    """
+    Copies a station from ~/source/RMS to ~/source/Stations, and if it is the first station, migrates ~/RMS_data/etc
+    to ~/RMS_data/$Station/etc
+
+    Generally safe to run multiple times, as can detect if a station has already been migrated
+
+    Args:
+        config_path:[path] path to config file
+        first_station: [bool] is this the first station
+        new_station_id: [str] id of new station
+        debug: [bool] prints debugging information
+
+    Returns:
+        nothing
+    """
+
+    # Get the path to the config file
+    config_path = os.path.expanduser(config_path)
+
+    # Check the config path exists and the use to creat stations
+    if os.path.exists(config_path):
+        config = cr.parse(config_path)
+
+        # If we are just migrating the first station, then use the stationID from the config file
+        new_station_id = config.stationID if new_station_id is None else new_station_id
+
+        # Work out the existing paths
+        platepar_path = os.path.expanduser(os.path.join(os.path.dirname(config_path), config.platepar_name))
+        mask_path = os.path.expanduser(os.path.join(os.path.dirname(config_path),config.mask_file))
+
+        if debug:
+            print("Platepar path {}, Mask path {}".format(platepar_path, mask_path))
+
+        # Get the new station paths
+        new_station_config_path, new_station_data_dir = computeNewStationPaths(config, new_station_id=new_station_id)
+
+        # If both of the new paths exist, then stop working on this station
+        if os.path.exists(new_station_config_path) and os.path.exists(new_station_data_dir):
+            if debug:
+                print("Station {} appears to have been migrated to multicam already, not doing any work".format(new_station_id))
+            return
+
+        # If first station is set then print some helpful information
+        if first_station:
+            print("It appears you have already configured your first station {}".format(new_station_id))
+            print("so its config files will now be relocated to {:s}".format(new_station_config_path))
+            print("Captured data from your original station is stored in {}".format(config.data_dir))
+            print("This data will be moved to {:s}".format(new_station_data_dir))
+
+            # Get rid of old icons
+            cleanDesktop()
+
+            # Has the data_dir already been created
+            if os.path.exists(new_station_data_dir):
+                print("Data directory for {} has already been migrated".format(new_station_id))
+            else:
+                # If it has not then work out where the existing archived and captured dir should be
+                expected_path_of_captured_dir = os.path.join(config.data_dir, config.captured_dir)
+                expected_path_of_archived_dir = os.path.join(config.data_dir, config.archived_dir)
+
+                # Check that everything is where it should be before we start work
+                if os.path.exists(expected_path_of_archived_dir) and os.path.exists(expected_path_of_captured_dir):
+
+                    # For the first station only, we have to rename the whole RMS_data directory
+                    # Strategy is to rename RMS_data to the name of the new station
+                    # Then create a new RMS_data directory
+                    # Then move the renamed directory into RMS_data
+
+                    temp_rms_data_path = os.path.join(os.path.dirname(config.data_dir), config.stationID.lower())
+                    if debug:
+                        print("Temporary data path for first station {}".format(temp_rms_data_path))
+                    moveIfExists(config.data_dir, temp_rms_data_path, debug=False)
+                    if debug:
+                        print("Final destination for first station {}".format(new_station_data_dir))
+                    moveIfExists(temp_rms_data_path, new_station_data_dir, debug=False)
+                else:
+                    #
+                    if debug:
+                        print("Cannot find expected directories for {} in {}".format(new_station_id, config.data_dir))
+
+                if os.path.exists(new_station_config_path):
+                    if debug:
+                        print("Config for {} has already been migrated".format(new_station_id))
+                else:
+                    mkdirP(new_station_config_path)
+                    copyIfExists(config_path, new_station_config_path, debug=False)
+
+
+        else:
+            # This is not the first station
+            # Migrate the .config file
+            if os.path.exists(new_station_config_path):
+                if debug:
+                    print("Config for {} has already been migrated".format(new_station_id))
+            else:
+                mkdirP(new_station_config_path)
+                copyIfExists(config_path, new_station_config_path, debug=True)
+
+        # Move the platepar
+        copyIfExists(platepar_path, new_station_config_path, debug=True)
+
+        # Move the mask
+        if os.path.exists(os.path.join(new_station_config_path,os.path.basename(mask_path))):
+            if debug:
+                print("Mask already migrated to {}".format(new_station_config_path))
+        else:
+            if moveIfExists(mask_path, new_station_config_path):
+                pass
+            else:
+                getMaskFromMaster(dest_path=new_station_config_path)
+
+        # Finish off by creating desktop shortcuts
+        createDesktopShortcuts(config.stationID)
+        desktop_path = checkUserDesktopDirectoryEnvironment()
+        if desktop_path != "":
+            if debug:
+                print("Desktop environment variable found as {}".format(desktop_path))
+        else:
+            if debug:
+                print("No desktop environment variable found")
+        # whilst adding stations set extra_space as 40GB
+        extra_space_gb = 40
+
+        customiseConfig(new_station_config_path, new_station_id.upper(), new_station_data_dir,
+                                                    extra_space_gb, reboot_after_processing=False)
+
+        makeKeys()
+
+if __name__ == "__main__":
+
+
+    arg_parser = argparse.ArgumentParser(description=""" Deleting old observations.""")
+    arg_parser.add_argument('-s', '--stations', nargs='*', metavar='STATIONS_TO_ADD', type=str, help="STATIONS_TO_ADD")
+    cml_args = arg_parser.parse_args()
+
+    ignore_hardware = False
+
+    station_list = []
+    if cml_args.stations is not None:
+        stations_list = cml_args.stations
+
+    if isPi5() or ignore_hardware:
+        print("Hardware reported as {}, continuing".format(piModel()))
+    else:
+        print("Pi 5 or similar required for multicamera operation, quitting")
+        exit(1)
+
+    # Is a station already configured
+    if not firstStationConfigured():
+        # Get operator input to configure first station
+        configureFirstStation()
+    # Copy the first station to new location
+    copyPiStation(first_station=True)
+
+    # Get the list of station
+    station_list = getStationsToAdd(stations_list)
+
+    # Work through the list of stations
+    for entry in station_list:
+        s=entry.lower()
+        copyPiStation(new_station_id=s, first_station=False)
+
+    # This prevents gui from placing windows directly on top of each other
+    uncomment("~/.config/wayfire.ini", "mode")
+
+    # Create .ssh keys if they do not exist
+    print(makeKeys(copy_pub_to="~/Desktop"))
+
+    # Set timezone to UTC
+    setTimeZone()
+
+    # Compute disc use quotas
+    quotas = computeQuotas()
+
+    # And apply disc use quotas
+    for entry in station_list:
+        path_to_config = os.path.join(os.path.expanduser("~/source/Stations"),entry.lower())
+        setQuotas(path_to_config, quotas)
