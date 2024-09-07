1cb51ecbed521490a1a557aacc786405150a0673
David Rollinson <g7gpr@outlook.com>
2024-09-07 13:10:52 +0000

    Add launch script

--- Scripts/MultiCamLinux/Pi/RMS_StartCapture_MCP.sh
+++ Scripts/MultiCamLinux/Pi/RMS_StartCapture_MCP.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo " Starting all configured stations post-update..."
-for Dir in ~/source/Stations/*
-  do
-	Station=$(basename $Dir)
-	echo "Starting camera ${Station}"
-	lxterminal --title=${Station} -e "$HOME/source/RMS/Scripts/MultiCamLinux/StartCapture.sh ${Station}"  &
-	sleep 30
-  done
+source ~/vRMS/bin/activate
+python -m Utils.MultiCamAddPiStation -l
+
