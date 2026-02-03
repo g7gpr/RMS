eb8df390e2505e1f1d3bf9ff9f6f6f8d6c33e786
g7gpr <g7gpr@outlook.com>
2026-02-03 00:13:53 +0000

    Shorten long lines

--- RMS/EventMonitor.py
+++ RMS/EventMonitor.py
@@ -2412,22 +2412,23 @@ class EventMonitor(multiprocessing.Process):
                 time_left_before_start_minutes = int(time_left_before_start.total_seconds()/60)
                 next_check_start_time = (RmsDateTime.utcnow() + datetime.timedelta(minutes=self.check_interval))
                 next_check_start_time_str = next_check_start_time.replace(microsecond=0).strftime('%H:%M:%S')
-                log.info('Next EventMonitor run : {} UTC; {:03} minutes from now'.format(next_check_start_time_str,
-                                                                                         round(self.check_interval)))
+                log.info('Next EventMonitor run : {} UTC; {:05.1f} minutes from now'.format(next_check_start_time_str,
+                                                                                         self.check_interval))
                 if time_left_before_start_minutes < 120:
-                    log.info('Next Capture start    : {} UTC; {:03} minutes from now'.format(str(start_time.strftime('%H:%M:%S')),
-                                                                                             round(time_left_before_start_minutes)))
+                    log.info('Next Capture start    : {} UTC; {:05.1f} minutes from now'.format(str(start_time.strftime('%H:%M:%S')),
+                                                                                             time_left_before_start_minutes))
                 else:
                     log.info('Next Capture start    : {} UTC'.format(str(start_time.strftime('%H:%M:%S'))))
             else:
                 next_check_start_time = (RmsDateTime.utcnow() + datetime.timedelta(minutes=self.check_interval))
                 next_check_start_time_str = next_check_start_time.replace(microsecond=0).strftime('%H:%M:%S')
-                log.info('Next EventMonitor run : {} UTC {:03} minutes from now'.format(next_check_start_time_str, round(self.check_interval)))
+                log.info('Next EventMonitor run : {} UTC {:05.1f} minutes from now'.format(next_check_start_time_str,
+                                                                                           self.check_interval))
                 end_time = RmsDateTime.utcnow() + datetime.timedelta(seconds=duration)
                 time_left_before_end_minutes = round(duration / 60, 1)
                 if time_left_before_end_minutes < 120:
-                    log.info('Next Capture end      : {} UTC; {:03} minutes from now'.format(str(end_time.strftime('%H:%M:%S')),
-                                                                                             round(time_left_before_end_minutes)))
+                    log.info('Next Capture end      : {} UTC; {:05.1f} minutes from now'.format(str(end_time.strftime('%H:%M:%S')),
+                                                                                             time_left_before_end_minutes))
             # Wait for the next check
             self.exit.wait(60*self.check_interval)
             # Increase the check interval
