cc868da080d56fece12ad4e72c42903cfeb569b1
g7gpr <g7gpr@outlook.com>
2026-02-03 00:09:21 +0000

    Restore sleep

--- RMS/EventMonitor.py
+++ RMS/EventMonitor.py
@@ -2390,7 +2390,7 @@ class EventMonitor(multiprocessing.Process):
         # Delay to allow capture to check existing folders - keep the logs tidy
 
 
-        time.sleep(1)
+        time.sleep(60)
         last_check_start_time = RmsDateTime.utcnow()
         while not self.exit.is_set():
             check_start_time = RmsDateTime.utcnow()
