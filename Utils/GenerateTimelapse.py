8a300864bb5cd786d886238c806539936eb8f333
g7gpr <g7gpr@outlook.com>
2026-02-05 01:45:57 +0000

    Refactor

--- Utils/GenerateTimelapse.py
+++ Utils/GenerateTimelapse.py
@@ -159,10 +159,11 @@ def generateTimelapse(dir_path, keep_images=False, fps=None, output_file=None, h
                 frames_processed_each_second = 1
             frames_remaining_to_process = total_ff - c
             remaining_time_seconds = frames_processed_each_second * frames_remaining_to_process
+            remaining_time_str = str(timedelta(seconds=remaining_time_seconds))
             estimated_completion_time  = RmsDateTime.utcnow() + timedelta(seconds = remaining_time_seconds)
 
             print("{:>5d}/{:>5d}, Elapsed: {:s}, Remaining: {:s}, Completion Time: {:s}"
-                  .format(c, total_ff, str(elapsed_time), str(remaining_time_seconds), str(estimated_completion_time)), end="\r")
+                  .format(c, total_ff, str(elapsed_time), str(remaining_time_str), str(estimated_completion_time)), end="\r")
             sys.stdout.flush()
 
     # now make the timelapse
