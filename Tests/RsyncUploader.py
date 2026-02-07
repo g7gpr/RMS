3220494394e2399ab987cff4f30b3748fdd08406
g7gpr <g7gpr@outlook.com>
2026-02-07 12:43:51 +0000

    Reduce logging

--- Tests/RsyncUploader.py
+++ Tests/RsyncUploader.py
@@ -138,6 +138,7 @@ def makeUpload(config_dict, return_after_each_upload=False):
                     break
 
             for station in config_dict:
+                log.info(f"\t\tfor {station}")
                 config = config_dict[station]
                 station_id = config.stationID
                 station_id_lower = station_id.lower()
