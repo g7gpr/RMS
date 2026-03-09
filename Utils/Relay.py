f48395ade7f4534ea38ec5fdb7fb6b107af34b43
David Rollinson <g7gpr@outlook.com>
2026-03-09 09:07:39 +0000

    Fix data rate calculation

--- Utils/Relay.py
+++ Utils/Relay.py
@@ -487,11 +487,11 @@ if __name__ == '__main__':
                             i += 1
                             upload_success, mb_sent, lag_time, lag_time_str, remote_file_path, data_rate, int_ts = uploadFile(station, f, sftp, test=False)
 
-                            log_line = f"{mb_sent:6.1f}MB to {station}@{HOSTNAME}:{remote_file_path} in {int_ts:03d} seconds at {data_rate:3.2f}MB/s - delay {lag_time_str}"
+                            log_line = f"{mb_sent:6.1f}MB to {station}@{HOSTNAME}:{remote_file_path} in {int_ts:03d} seconds at {data_rate:01.2f}MB/s - delay {lag_time_str}"
                             data_sent += mb_sent
                             this_station_seconds = int((datetime.datetime.now() - start_station_time).total_seconds())
                             data_rate_so_far_this_station = data_sent / this_station_seconds
-                            log_line += f" ({i}/{len(files_to_upload)}) {this_station_seconds} seconds on this station to send {data_sent:.1f}MB at {data_rate_so_far_this_station:.1f}MB/s"
+                            log_line += f" ({i}/{len(files_to_upload)}) {this_station_seconds:03d} seconds cumulative to send {data_sent:04.1f}MB at {data_rate_so_far_this_station:01.2f}MB/s"
 
                             log.info(log_line)
 
@@ -553,7 +553,7 @@ if __name__ == '__main__':
 
         log.info(f"Maximum lag time {max_lag_time_across_stations}")
         log.info(f"Previous maximum lag time {previous_max_lag_time_across_stations}")
-        log.info(f"Difference {previous_max_lag_time_across_stations - max_lag_time_across_stations}")
+        log.info(f"Difference {max_lag_time_across_stations  - previous_lag_time_across_stations}")
         log.info(f"Lag warning threshold {LAG_WARNING_THRESHOLD}")
         log.info(f"Lag warning deadband {LAG_WARNING_DEADBAND}")
 
