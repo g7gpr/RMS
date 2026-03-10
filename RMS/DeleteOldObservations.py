18c276c093e89ce159da63c58baa8cd267b2fcd4
g7gpr <g7gpr@outlook.com>
2026-03-10 06:37:50 +0000

    Fix indenting - delete folders relating to detected files

--- RMS/DeleteOldObservations.py
+++ RMS/DeleteOldObservations.py
@@ -799,10 +799,11 @@ def deleteFilesHeldOnServer(config, verbose=False):
         if f.split("_")[4].startswith("detected") or f.split("_")[4].startswith("metadata") or f.split("_")[4].startswith("imgdata"):
             if os.path.exists(path_to_delete):
                 full_paths_to_files_to_delete_list.append(path_to_delete)
-                directory = "_".join(f.split("_")[0:4])
-                directory = os.path.join(archived_dir, directory)
+
             # If it is a detected file, then we can delete the associated archive directory
             if f.split("_")[4].startswith("detected"):
+                directory = "_".join(f.split("_")[0:4])
+                directory = os.path.join(archived_dir, directory)
                 if os.path.exists(directory):
                     if os.path.isdir(directory):
                         full_paths_to_dirs_to_delete_list.append(directory)
