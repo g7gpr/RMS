15624e18dfbe147a3e59c56816b646d14fbca47b
g7gpr <g7gpr@outlook.com>
2026-05-13 14:29:52 +0000

    Log permission changes

--- Utils/StarPipeline/Ingest.py
+++ Utils/StarPipeline/Ingest.py
@@ -1336,6 +1336,7 @@ def moveFiles(local_target, path_source_list, path_local_list):
 
     files_available = []
     mkdirP(local_target)
+    log.info(f"Setting permissions on {local_target}")
     os.chmod(os.path.dirname(local_target), 0o777)
     os.chmod(local_target, 0o777)
     for p_source, p_local in zip(path_source_list, path_local_list):
